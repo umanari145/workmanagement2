f44d6689917ac700643feb2319a54714f7d9367f
umanari145 <umanari145@gmail.com>
2021-05-04 22:25:03 +0900

    最初の画面を作成

--- src/api/api.config.js
+++ src/api/api.config.js
@@ -1,2 +1,2 @@
-export const dev_api_url = 'https://us-central1-dummy-80371.cloudfunctions.net/v1/';
-export const prod_api_url ='https://us-central1-dummy-80371.cloudfunctions.net/v1/';
+export const dev_api_url = '**************';
+export const prod_api_url ='https://aaaaaaaaaaa/v1/';
--- src/store/master_list.js
+++ src/store/master_list.js
@@ -2,10 +2,14 @@
 const master_list = {
     namespaced: true,
     state:{
-        master_list:[]
+        master_list:{
+            "room_list":''
+        }
     },
     getters:{
         get_master_list(state) {
+            let room_list = {"1":"市川", "2":"福岡"};
+            state.master_list.room_list = room_list;
             return state.master_list;
         },
     },
